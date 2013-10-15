1695d87865f56c01d2af765da7f0d48bb4159b50
cheenu <cheenu.madan@gmail.com>
2013-10-15 11:41:50 +0530

    Added disable gesture recognition commands

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,9 @@
 class AppDelegate < PM::Delegate
   def on_load(app, options)
     open_slide_menu NavScreen, HomeScreen.new(nav_bar: true)
+
+    slide_menu.recognizesPanningOnFrontView = false
+    App.delegate.slide_menu.recognizesPanningOnFrontView = false
+    app_delegate.slide_menu.recognizesPanningOnFrontView = false
   end
 end
